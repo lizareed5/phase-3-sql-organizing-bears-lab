d16d842481fdbef9d6301631fff8a54f7a168b1a
Liza McLain <lizamclain@gmail.com>
2023-02-01 18:10:51 -0600

    commit tree e1ef2fa4785e5ae292f02804516f83c83652c8c8

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'chocolate brown', 'loveable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'brown', 'energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'black', 'motherly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 16, 'M', 'light brown', 'legendary', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'M', 'cinnamon brown', 'shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 13, 'F', 'black and brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'brown', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'M', 'midnight black', 'mysterious', 1);
